af1cda6eb5857715b90ae70f04e2d2a676e14cb0
Artem Pshegodskii <pshegiya@gmail.com>
2022-08-16 14:40:29 -0700

    first commit

---
+++ base/views.py
@@ -0,0 +1,52 @@
+from pydoc_data.topics import topics
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
+
+
+def home(request):
+    rooms = Room.objects.all()
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics':topics}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request,'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+       form = RoomForm(request.POST)
+       if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, istance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
